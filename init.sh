a894bd9346e77b1706bbcf1fe0067cc76f196e5c
Huangshan Chen <chen@kip.uni-heidelberg.de>
2018-04-11 00:51:11 +0200

    add a script to set skip-workstree to standalone_test and separated_pages files

---
+++ init.sh
@@ -0,0 +1,7 @@
+# initial operation for newly cloned repository
+
+# instruct git not to touch the standalone_test.tex and seperated_pages.tex.
+# these two files are changed from time to time and the changes of these two
+# files should not be commited.
+git update-index --skip-worktree standalone_test.tex
+git update-index --skip-worktree seperated_pages.tex
